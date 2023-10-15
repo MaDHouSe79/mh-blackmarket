abaa762228b88d63ebc6119f7b9f1423de7edab6
MaDHouSe79 <madhouse1979@gmail.com>
2023-10-15 15:31:57 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -6,7 +6,7 @@ local Translations = {
         ['no_blackmoney'] = "You don't have any black money with you...",
     },
     target = {
-        ['talk_to'] = "Talk to STEVE-O",
+        ['talk_to'] = "Talk to MaDHouSe",
     }
 }
 
