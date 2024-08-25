a953feb6a2c914bda70ee9525bcf5fa17d5ce058
MaDHouSe <madhouse1979@gmail.com>
2024-08-25 22:17:05 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -4,13 +4,17 @@
 local Translations = {
     notify = {
         ['no_blackmoney'] = "You don't have any black money with you...",
+        ['shop_not_found'] = "Shop does not exist",
     },
     target = {
         ['talk_to'] = "Talk to MaDHouSe",
     }
 }
 
-Lang = Locale:new({
-    phrases = Translations, 
-    warnOnMissing = true
-})
+if GetConvar('qb_locale', 'en') == 'en' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
