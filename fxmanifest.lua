6ff47fef76ef0c1a302f31e6eae4833121685f5c
MaDHouSe <madhouse1979@gmail.com>
2024-08-25 22:21:44 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,8 +8,8 @@ author 'MaDHouSe'
 description 'mh-blackmarket'
 version '1.0'
 
-shared_scripts {'@qb-core/shared/locale.lua', 'locales/en.lua', 'config.lua'}
-client_scripts {'client/main.lua'}
-server_scripts {'server/main.lua', 'server/update.lua'}
+shared_scripts {'@qb-core/shared/locale.lua', 'locales/en.lua'}
+client_scripts {'configs/cl_config.lua', 'client/main.lua'}
+server_scripts {'configs/sv_config.lua', 'server/main.lua', 'server/update.lua'}
 
 lua54 'yes'
