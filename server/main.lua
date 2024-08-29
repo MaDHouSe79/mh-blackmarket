307b954f12691773403cf7c7ee2789285ac38d08
MaDHouSe <madhouse1979@gmail.com>
2024-08-29 17:25:04 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -41,9 +41,3 @@ RegisterServerEvent('mh-blackmarket:server:openShop', function(shopData)
         end
     end
 end)
-
-QBCore.Commands.Add('blackmoney', 'Check Blackmoney Balance', {}, false, function(source, _)
-    local Player = QBCore.Functions.GetPlayer(source)
-    local amount = Player.PlayerData.money.black_money
-    TriggerClientEvent('hud:client:ShowAccounts', source, 'black_money', amount)
-end)
