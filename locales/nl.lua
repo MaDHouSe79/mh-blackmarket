b6e50c9013f9f79e606e8bc96ac4c8b4d3209d74
MaDHouSe <madhouse1979@gmail.com>
2024-09-09 23:15:51 +0200

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -11,10 +11,8 @@ local Translations = {
     }
 }
 
-if GetConvar('qb_locale', 'en') == 'nl' then
-    Lang = Locale:new({
-        phrases = Translations,
-        warnOnMissing = true,
-        fallbackLang = Lang,
-    })
-end
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true,
+    fallbackLang = Lang,
+})
